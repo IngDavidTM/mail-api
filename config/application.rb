2dd99fb26863177a507ce5a6663cab4ce641aaca
IngDavidTM <david-1011@hotmail.com>
2024-07-03 12:05:45 -0500

    Remove extra line

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module MailApi
   class Application < Rails::Application
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://david-tamayo.netlify.app/'
+        origins 'https://david-tamayo.netlify.app'
         resource '*',
                  headers: :any,
                  methods: %i[get post put patch delete options head]
