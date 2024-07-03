b43cb2b9d7bfce65c5665edd7528eb4d18d4a15b
IngDavidTM <david-1011@hotmail.com>
2024-07-02 21:35:05 -0500

    Add support for external mails

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -1,9 +1,11 @@
 class EmailsController < ApplicationController
   def send_email
+    name = params[:name]
     email = params[:email]
     message = params[:message]
 
-    UserMailer.welcome_email(email, message).deliver_now
+    UserMailer.welcome_email(email).deliver_now
+    UserMailer.recruiter_email(name, email, message).deliver_now
 
     render json: { status: 'Email sent successfully' }
   end
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: 'david.tamayo1112@gmail.com'
 
-  def welcome_email(email, message)
-    mail(to: email, subject: 'Welcome to My Awesome Site', body: message, content_type: 'text/plain')
+  def welcome_email(email)
+    subject = 'Thank You for Reaching Out'
+    body = "Dear Recruiter,\n\nThank you for reaching out. I have received your email and will review it shortly. Looking forward to the opportunity to discuss further.\n\nBest regards,\nDavid Tamayo"
+    mail(to: email, subject: subject, body: body, content_type: 'text/plain')
+  end
+
+  def recruiter_email(name, email, message)
+    body_recruiter = "This is the message:\n\n#{message}\n\nBest regards,\n#{name} #{email}"
+    mail(to: 'david.tamayo1112@gmail.com', subject: 'Site Message', body: body_recruiter, content_type: 'text/plain')
   end
 end
